58cf0dd757e735f8064a0387b7509093edc1ea5e
HasibullahAman <hasib.aman7@gmail.com>
2024-09-16 08:44:57 -0400

    add Vazir font in project

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import './App.css';
 const App = () => {
   return (
     <div className="App">
-      <h3>مدریت مخاطبین</h3>
+      <h3 style={{fontFamily: "Vazir"}}>مدریت مخاطبین</h3>
       <button className='btn btn-primary'>Button</button>
+      <span className='fa fa-user'></span>
+      <span className='fas fa-address-book'></span>
     </div>
   );
 }
