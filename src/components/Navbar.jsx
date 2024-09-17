751bb8cff39794f29933be6d688210832a6804f2
HasibullahAman <hasib.aman7@gmail.com>
2024-09-17 04:47:39 -0400

    done color plate and navbar style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,15 +3,20 @@ import { PURPLE, BACKGROUND } from "../helpers/color";
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-dark navbar-expand-sm shadow-lg">
+    <nav
+      className="navbar navbar-dark navbar-expand-sm shadow-lg"
+      style={{ backgroundColor: BACKGROUND }}
+    >
       <div className="container">
         <div className="row w-100">
-        <div className="col ">
+          <div className="col ">
             <SearchContact />
           </div>
           <div className="col">
-            ویب آپلیکشن مدریت <span style={{ color: "purple" }}> مخاطبین</span>
-            <i className="fas fa-id-badge" />
+            <div className="navbar-brand">
+              ویب آپلیکشن مدریت <span style={{ color: PURPLE }}> مخاطبین</span> {" "}
+              <i className="fas fa-id-badge" style={{ color: PURPLE }} />
+            </div>
           </div>
         </div>
       </div>
--- src/components/contact/SearchContact.jsx
+++ src/components/contact/SearchContact.jsx
@@ -1,17 +1,19 @@
+import { PURPLE} from "../../helpers/color";
+
+
 const SearchContact = () => {
   return (
     <div className="input-group mx-2 w-75" dir="ltr">
       <span
         className="input-group-text"
         id="basic-addon1"
-        style={{ backgroundColor: "purple" }}
+        style={{ backgroundColor: PURPLE}}
       >
         <i className="fas fa-search" />
       </span>
       <input
         dir="rtl"
         type="text"
-        style={{ backgroundColor: "gray", borderColor: "purple" }}
         className="form-control"
         placeholder="جستوجوی مخاطبین"
         aria-label="search"
