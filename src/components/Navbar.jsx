5e986008a15784f8f8643f6838ebdbbb7fab75c5
HasibullahAman <hasib.aman7@gmail.com>
2024-09-17 04:19:31 -0400

    sync color plate

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,17 @@
+import SearchContact from "./contact/SearchContact";
+import { PURPLE, BACKGROUND } from "../helpers/color";
+
 const Navbar = () => {
   return (
     <nav className="navbar navbar-dark navbar-expand-sm shadow-lg">
       <div className="container">
         <div className="row w-100">
-          <div className="col">
-            <i className="fas fa-id-badge" />
-            به مدریت آپلیکشن <span style={{ color: "purple" }}> مخاطبین</span>
+        <div className="col ">
+            <SearchContact />
           </div>
-          <div className="col ">
-            <div className="input-group mx-2 w-75" dir="ltr">
-              <span
-                className="input-group-text"
-                id="basic-addon1"
-                style={{ backgroundColor: "purple" }}
-              >
-                <i className="fas fa-search" />
-              </span>
-              <input
-                dir="rtl"
-                type="text"
-                style={{ backgroundColor: "gray", borderColor: "purple" }}
-                className="form-control"
-                placeholder="جستوجوی مخاطبین"
-                aria-label="search"
-                aria-describedby="basic-addon1"
-              ></input>
-            </div>
+          <div className="col">
+            ویب آپلیکشن مدریت <span style={{ color: "purple" }}> مخاطبین</span>
+            <i className="fas fa-id-badge" />
           </div>
         </div>
       </div>
