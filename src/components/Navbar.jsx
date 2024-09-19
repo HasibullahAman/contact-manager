a99bfcea04965e6ac49c4f5d10988fc9a8f3d062
HasibullahAman <hasib.aman7@gmail.com>
2024-09-19 13:55:07 -0400

    start conditional formating

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,14 +9,14 @@ const Navbar = () => {
     >
       <div className="container">
         <div className="row w-100">
-          <div className="col ">
-            <SearchContact />
-          </div>
-          <div className="col">
+        <div className="col">
             <div className="navbar-brand">
               ویب آپلیکشن مدریت <span style={{ color: PURPLE }}> مخاطبین</span> {" "}
               <i className="fas fa-id-badge" style={{ color: PURPLE }} />
             </div>
+          </div>
+          <div className="col ">
+            <SearchContact />
           </div>
         </div>
       </div>
